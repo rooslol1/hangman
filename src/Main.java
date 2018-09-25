bb379a77ba0d98914860f84c90bb3b8a2d646b4c
rooslol1 <25539@web.nl>
2018-09-25 16:27:21 +0200

    goed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
-Import java.util.scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //write your code here
+        System.out.println("hello") ;
+
+
+        //datatypes: String, int, double, float, boolean, byte
+
+
+        String henk = "henk";
+        String Henk = "Henk";
+
+        henk = "piet";
+
+        System.out.println ("Henk");
+
+        int leeftijd = 16;
+        leeftijd = leeftijd + 6;
+        leeftijd +=6;
+        leeftijd++;
+        //leeftijd++;
+
+        float snelstetijd = 56.66f;
+        double nogpreciezer = 56.6666666666666666666666d;
+
+        System.out.println(leeftijd);
+
+        boolean snaphet = true; if(snaphet)
+            (
+
+                System.out.println("ik snap het");
+                )
+
+
+
+
+
+
+
+
+
 
     }
 }
